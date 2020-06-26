8f0216b13b2341de3f38efada0a2010a67ae5bdd
“JesusCanales” <jcanales@vallegrande.edu.pe>
2020-06-26 07:23:39 -0500

    Hemos agregado la tabla PERSONA a la base de datos

---
+++ Tables/dbo.PERSONA.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[PERSONA]
+(
+[CODPER] [int] NOT NULL,
+[NOMPER] [varchar] (100) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[APEPATPER] [varchar] (100) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[APEMATPER] [varchar] (100) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[DNIPER] [char] (8) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[TELEFPER] [char] (9) COLLATE Modern_Spanish_CI_AS NULL,
+[DOMPER] [varchar] (100) COLLATE Modern_Spanish_CI_AS NULL,
+[LUGDOMPER] [char] (6) COLLATE Modern_Spanish_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[PERSONA] ADD CONSTRAINT [PK_PERSONA] PRIMARY KEY CLUSTERED  ([CODPER]) ON [PRIMARY]
+GO
